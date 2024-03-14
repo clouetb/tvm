06c4e2da2bf3afcaca96e9984672f3c9506f9f31
Benoît Clouet <benoit.clouet@gmail.com>
2024-03-15 00:39:40 +0100

    Update detect_target.py for adding support for opencl
    Again, that was quite easy...

--- python/tvm/target/detect_target.py
+++ python/tvm/target/detect_target.py
@@ -58,6 +58,17 @@ def _detect_rocm(dev: Device) -> Target:
     )
 
 
+def _detect_opencl(dev: Device) -> Target:
+    return Target(
+        {
+            "kind": "opencl",
+            "max_shared_memory_per_block": dev.max_shared_memory_per_block,
+            "max_threads_per_block": dev.max_threads_per_block,
+            "thread_warp_size": dev.warp_size,
+        }
+    )
+
+
 def _detect_vulkan(dev: Device) -> Target:
     f_get_target_property = get_global_func("device_api.vulkan.get_target_property")
     return Target(
@@ -128,4 +139,5 @@ SUPPORT_DEVICE = {
     "metal": _detect_metal,
     "vulkan": _detect_vulkan,
     "rocm": _detect_rocm,
+    "opencl": _detect_opencl,
 }
